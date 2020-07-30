429445d54b6ae581dbcc74387a1096e76ced8209
Rupam <hrupam55@gmail.com>
2020-07-30 21:46:19 +0530

    Changed burger toggle direction

--- main.js
+++ main.js
@@ -7,17 +7,18 @@ burger.addEventListener('click', () => {
     // TOGGLE NAV
     nav.classList.toggle('nav-active');
 
+    // TOGGLE BURGER ICON
     burger.classList.toggle('burger-toggle');
 
     // LINK ANIMATION
-    let delay=0;
+    let delay = 0;
     navLinks.forEach(link => {
         if (link.style.animation) {
             link.style.animation = '';
         }
         else {
-            link.style.animation = `navLinkFade 0.7s ease-in-out forwards ${delay}s`;
+            link.style.animation = `navLinkFade 0.3s ease-in-out forwards ${delay}s`;
         }
-        delay+=0.2;
+        delay += 0.2;
     })
 })
