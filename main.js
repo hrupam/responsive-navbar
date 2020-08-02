8615067b21a055446d117b4bde5cf1727f2a0e92
Rupam <hrupam55@gmail.com>
2020-08-02 14:47:46 +0530

    Implemented jQuery

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li');
+$("nav").ready(function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-burger.addEventListener('click', () => {
+    $('.logo').addClass('animate__animated animate__bounceInRight');
 
-    // TOGGLE NAV
-    nav.classList.toggle('nav-active');
+    burger.addEventListener('click', () => {
 
-    // TOGGLE BURGER ICON
-    burger.classList.toggle('burger-toggle');
+        // TOGGLE NAV
+        nav.classList.toggle('nav-active');
 
-    //CUSTOM LINK ANIMATION
-    let delay = 0;
-    navLinks.forEach(link => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        }
-        else {
-            link.style.animation = `navLinkFade 0.3s ease-in-out forwards ${delay}s`;
-        }
-        delay += 0.2;
+        // TOGGLE BURGER ICON
+        burger.classList.toggle('burger-toggle');
+
+        //CUSTOM LINK ANIMATION
+        let delay = 0;
+        navLinks.forEach(link => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.3s ease-in-out forwards ${delay}s`;
+            }
+            delay += 0.2;
+        })
+
+        // USING ANIMATE.CSS
+        // navLinks.forEach(link =>{
+        //     link.classList.toggle('animate__animated');
+        //     link.classList.toggle('animate__bounceInLeft');
+        // })
+
+
     })
-
-    // USING ANIMATE.CSS
-    // navLinks.forEach(link =>{
-    //     link.classList.toggle('animate__animated');
-    //     link.classList.toggle('animate__bounceInLeft');
-    // })
-
-
 })
+
+
