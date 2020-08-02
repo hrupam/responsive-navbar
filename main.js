1bc22f1a5f5dd144363aa139053feb3bb7ec0c41
Rupam <hrupam55@gmail.com>
2020-08-02 20:11:36 +0530

    minor change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$("nav").ready(function () {
+$(document).ready(() => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
